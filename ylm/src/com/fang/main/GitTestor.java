a7e43876b9543af4db97c900b49c5535662cae5f
wangguofang <gfang@qq.com>
2015-10-15 17:36:47 +0800

    For Test

---
+++ ylm/src/com/fang/main/GitTestor.java
@@ -0,0 +1,10 @@
+package com.fang.main;
+
+public class GitTestor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Hello Git !" );
+	}
+
+}
